f5f9e54b718aba0f0659211abccf270c156c0428
Silkaleex <alejandropascual1995@outlook.es>
2022-06-18 16:18:41 +0200

    mi-pagina-web

--- app/App.js
+++
@@ -1,7 +0,0 @@
-import React from "expo-status-bar";
-import Main from "./src/components/main";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return <Main />;
-}
